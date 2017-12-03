f8b856183a218b8c04c1272f2877fbbf9c9183b4
cp-hagiwarakeiichi <hagiwara.keiichi@cloudpayment.co.jp>
2017-12-03 20:55:43 +0900

    forms.pyとapps.pyを移行

--- recruitment/apps.py
+++ recruitment/apps.py
@@ -3,3 +3,9 @@ from django.apps import AppConfig
 
 class RecruitmentConfig(AppConfig):
     name = 'recruitment'
+
+class InterviewConfig(AppConfig):
+    name = 'interview'
+
+class ArchiveList(AppConfig):
+    name = 'archive'
---
+++ recruitment/forms.py
@@ -0,0 +1,24 @@
+from django.forms import ModelForm
+
+from recruitment.models import Recruitment,Interview,ArchiveList
+
+
+class RecruitmentForm(ModelForm):
+    class Meta:
+        model = Recruitment
+        fields = [
+            'name', 'email', 'school_name', 'department',
+            'school_year', 'age', 'graduation_year', 'selection_status'
+        ]
+
+class InterviewForm(ModelForm):
+    class Meta:
+        model = Interview
+        fields = [
+            'date', 'time', 'interviewer'
+        ]
+
+class ArchiveList(ModelForm):
+    class Meta:
+        model = ArchiveList
+        fields = ['user']
