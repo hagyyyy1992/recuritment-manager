959a2b3d383ac19ea6fa2e916a02d00619077b33
cp-hagiwarakeiichi <hagiwara.keiichi@cloudpayment.co.jp>
2017-12-03 20:28:07 +0900

    admin.pyにArchiveListを追加

--- recruitment/admin.py
+++ recruitment/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Recruitment,Interview
 
-# Register your models here.
+from .models import Recruitment,Interview,ArchiveList
+
 admin.site.register(Recruitment)
 admin.site.register(Interview)
+admin.site.register(ArchiveList)
