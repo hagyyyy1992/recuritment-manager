cbd81d7de7632cd4a675bd994b11258ce8d6a06f
cp-hagiwarakeiichi <hagiwara.keiichi@cloudpayment.co.jp>
2018-02-04 22:43:42 +0900

    暫定対応

--- recruitment/urls.py
+++ recruitment/urls.py
@@ -4,6 +4,7 @@ from recruitment import views
 urlpatterns = [
     url(r'^$', views.candidate_list, name='candidate_list'),  # 選考者一覧画面
     url(r'^(?P<candidate_id>[0-9]+)/candidate_edit/$', views.edit, name='candidate_edit'),  # 選考者編集画面
+    url(r'^(?P<candidate_id>[0-9]+)/candidate_edit/$', views.edit, name='candidate_edit'),  # アーカイブ編集画面
     url(r'^candidate_add/$', views.add_to_candidate_list, name='candidate_add'),  # 選考者追加画面
     url(r'^(?P<candidate_id>[0-9]+)/delete/$', views.delete, name='candidate_delete'),  # 削除ボタン
     url(r'^archive_list/$', views.archive_list, name='archive_list'),  # アーカイブ画面
