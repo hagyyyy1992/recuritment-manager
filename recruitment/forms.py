3c4bf5ed59be39646bd52a351608cce5f8339103
kqxgy385 <kqxgy385@gmail.com>
2018-07-01 11:56:32 +0900

    リフォーマット

--- recruitment/forms.py
+++ recruitment/forms.py
@@ -7,8 +7,14 @@ class RecruitmentForm(ModelForm):
     class Meta:
         model = Recruitment
         fields = [
-            'name', 'email', 'school_name', 'department',
-            'school_year', 'age', 'graduation_year', 'selection_status'
+            'name',
+            'email',
+            'school_name',
+            'department',
+            'school_year',
+            'age',
+            'graduation_year',
+            'selection_status'
         ]
 
 
@@ -16,11 +22,16 @@ class InterviewForm(ModelForm):
     class Meta:
         model = Interview
         fields = [
-            'recruitment', 'date', 'time', 'interviewer'
+            'recruitment',
+            'date',
+            'time',
+            'interviewer'
         ]
 
 
 class ArchiveList(ModelForm):
     class Meta:
         model = ArchiveList
-        fields = ['user']
+        fields = [
+            'user'
+        ]
