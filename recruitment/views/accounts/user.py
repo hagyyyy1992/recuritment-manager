7ccc85f620ee7c13e4e971675e774dfd4a0ef313
kqxgy385 <kqxgy385@gmail.com>
2018-07-28 23:35:52 +0900

    アカウント登録機能ができた

--- recruitment/views/accounts/user.py
+++ recruitment/views/accounts/user.py
@@ -1,19 +1,14 @@
-from django.contrib.auth import get_user_model
-from django.shortcuts import redirect
+from django.urls import reverse_lazy
 from django.views import generic
 
 from recruitment.forms import UserCreateForm
-
-User = get_user_model()
 
 
 class UserCreate(generic.CreateView):
     """ユーザー登録"""
     template_name = 'accounts/user_create.html'
     form_class = UserCreateForm
-
-    def form_valid(self, form):
-        return redirect('user_create_done')
+    success_url = reverse_lazy('user_create_done')
 
 
 class UserCreateDone(generic.TemplateView):
